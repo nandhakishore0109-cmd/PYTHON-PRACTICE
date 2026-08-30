8addc27a99cb929cc0d16475e2c40761a277c4cd
Nandha Kishore Thota <nandhakishore0109@gmail.com>
2026-08-30 23:50:34 +0530

    practice

---
+++ Basics.py
@@ -0,0 +1,87 @@
+#print('welcome to python')
+
+'''print("Hello Nandha")
+print("Welcome to Pytho")
+
+Name = "Nandha"
+age = 20
+cgpa = 9.3
+is_student=True
+print(age)
+print("Hi",Name)
+print(type(is_student))
+
+a=10
+b=10.5
+c="python"
+d=False
+print(type(a))
+print(type(b))
+print(type(c))
+
+num = "100"
+print(num)
+print(int(num))
+print(float(num))
+
+a="10"
+b=20
+print(int(a)+b)
+#print(a + b)
+print(float(a))
+print(type(a))
+print(type(int(a)))
+
+      
+# basics of variables
+
+num1 = int(input("enter a value:"))
+num2 = int(input("enter a value:"))
+num3 = int(input("enter a value:"))
+print(num1 + num3)
+print(num2 - num3)
+print(num1*num3)
+
+
+# LISTS.... 
+
+list = [1,20,'python']
+list.append('welcome to vscode')
+print(list)
+print(id(list))
+
+#TUPLES...
+
+tuple = (1,123.45,23,[1,34,'naidu'],'nandha kishore')
+#tuple.append('kishore')
+print(tuple)
+print(type(tuple))
+
+#SETS ....
+
+set = {1,'python',45.56,(1234,32,45),[1,2,45]}
+print(set)  # sets doesnt allows lists because its entering only immutable data types....
+
+#DICTIONARY  (COMBINATION OF KEYS:VALUES)
+
+dict = {"name":"nandu","age": 20,"name":"vasu"}
+print(dict)   #KEYS CONTAINS ONLY NTS DATATYPES BUT VALUES CONTAINS ANY DATATYPE
+
+#COMPLEX DATATYPE (a+bj)
+
+a = 3+4j
+print(type(a)) # it contains real part and imaginary part '''
+
+
+#OPERATORS...+,-,*,/,%
+#operator is performs operation
+#operand is value or variable 
+
+#num1 = 10
+#num2 = 20    
+#print(num1 + num2)  # '+' IS OPEARTOR(operation)
+
+
+
+
+
